6a8b88ab78325b44515aa37936837a245a186379
pmeletis <p_c_meletis@yahoo.com>
2021-05-13 01:04:09 +0200

    Several improvements.

--- panoptic_parts/utils/format.py
+++ panoptic_parts/utils/format.py
@@ -61,16 +61,18 @@ def _decode_uids_functors_and_checking(uids):
     divmod_ = np.divmod
     maximum = np.maximum
     dtype = np.int32
+    logical_and = np.logical_and
     _validate_uids_values_numpy_python(uids)
-    return where, ones_like, divmod_, maximum, dtype
+    return where, ones_like, divmod_, maximum, dtype, logical_and
   if isinstance(uids, (int, np.int32)):
     where = lambda cond, true_br, false_br: true_br if cond else false_br
     ones_like = lambda the_like: type(the_like)(1)
     divmod_ = divmod
     maximum = max
     dtype = (lambda x: x) if isinstance(uids, int) else np.int32
+    logical_and = (lambda x, y: x and y) if isinstance(uids, int) else np.logical_and
     _validate_uids_values_numpy_python(uids)
-    return where, ones_like, divmod_, maximum, dtype
+    return where, ones_like, divmod_, maximum, dtype, logical_and
 
   # optional frameworks: Tensorflow and Pytorch
   if TENSORFLOW_IMPORTED:
@@ -82,7 +84,8 @@ def _decode_uids_functors_and_checking(uids):
       divmod_ = lambda x, y: (x // y, x % y)
       maximum = tf.maximum
       dtype = lambda x: x
-      return where, ones_like, divmod_, maximum, dtype
+      logical_and = tf.logical_and
+      return where, ones_like, divmod_, maximum, dtype, logical_and
   if TORCH_IMPORTED:
     if isinstance(uids, torch.Tensor):
       where = torch.where
@@ -90,12 +93,13 @@ def _decode_uids_functors_and_checking(uids):
       divmod_ = lambda x, y: (x // y, x % y)
       maximum = torch.max
       dtype = functools.partial(torch.tensor, dtype=torch.int32)
-      return where, ones_like, divmod_, maximum, dtype
+      logical_and = torch.logical_and
+      return where, ones_like, divmod_, maximum, dtype, logical_and
 
   raise TypeError(f'{type(uids)} is an unsupported type of uids.')
 
 def decode_uids(uids, *, return_sids_iids=False, return_sids_pids=False,
-                experimental_null_id=-1, experimental_dataset_spec=None):
+                experimental_no_info_id=-1, experimental_dataset_spec=None):
   """
   Given the universal ids `uids` according to the hierarchical format described
   in README, this function returns element-wise
@@ -105,13 +109,13 @@ def decode_uids(uids, *, return_sids_iids=False, return_sids_pids=False,
   sids_iids represent the semantic-instance-level (two-level) labeling,
   e.g., sids_iids(Cityscapes-Panoptic-Parts) = Cityscapes-Original.
 
-  sids_pids = sids * 100 + max(pids, 0) and represent the semantic-part-level labeling,
+  sids_pids represent the semantic-part-level (semantics) labeling,
   e.g., sids_pids(23_003_04) = 23_04.
 
   Examples (output is same type as input - not shown for clarity):
-    - decode_uids(23) → (23, -1, -1)
-    - decode_uids(23003) → (23, 3, -1)
-    - decode_uids(2300304) → (23, 3, 4)
+    - decode_uids(23, return_sid_pid=True) → (23, -1, -1, 23)
+    - decode_uids(23003, return_sid_pid=True) → (23, 3, -1, 23)
+    - decode_uids(2300304, return_sid_pid=True) → (23, 3, 4, 2304)
     - decode_uids(tf.constant([1, 12, 1234, 12345, 123456, 1234567])) →
       ([ 1, 12,   1,  12,   1,  12],
        [-1, -1, 234, 345, 234, 345],
@@ -141,7 +145,7 @@ def decode_uids(uids, *, return_sids_iids=False, return_sids_pids=False,
     if return_sids_pids:
       sids_pids: same type and shape as uids, will have no -1.
   """
-  where, ones_like, divmod_, maximum, dtype = _decode_uids_functors_and_checking(uids)
+  where, ones_like, divmod_, maximum, dtype, logical_and = _decode_uids_functors_and_checking(uids)
 
   # explanation for using dtype and np.asarray in this function:
   #   dtype: numpy implicitly converts Python int literals in np.int64, we need np.int32
@@ -151,17 +155,19 @@ def decode_uids(uids, *, return_sids_iids=False, return_sids_pids=False,
   # TODO(panos): any uid that is invalid according to dataset_spec must be mapped to unlabeled
   # e.g. invalid pid=5 for CPP rider and person: np.logical_and(np.logical_or(sids == 24, sids == 25), pids == 5)
 
-  # pad uids to uniform 7-digit length
+  # pad uids to uniform 7-digit length, this generates a fake iid (0) for uids <= 99,
+  # but this is handled later when iids are calculated
   uids_padded = where(uids <= 99_999,
                       where(uids <= 99, uids * dtype(10**5), uids * dtype(10**2)),
                       uids)
   # split uids to components (sids, iids, pids) from right to left
   sids_iids, pids = divmod_(uids_padded, dtype(10**2))
   sids, iids = divmod_(sids_iids, dtype(10**3))
-  invalid_ids = ones_like(uids) * dtype(experimental_null_id)
+  noinfo_ids = ones_like(uids) * dtype(experimental_no_info_id)
   # set invalid ids
-  iids = where(uids <= 99, invalid_ids, iids)
-  pids = where(uids <= 99_999, invalid_ids, pids)
+  iids = where(uids <= 99, noinfo_ids, iids)
+  # pids bearing no information are also those which have pid = 0
+  pids = where(logical_and(uids <= 99_999, pids == dtype(0)), noinfo_ids, pids)
 
   # This applies to PASCAL Panoptic Parts and removes the
   # part-level instance information layer
@@ -173,18 +179,15 @@ def decode_uids(uids, *, return_sids_iids=False, return_sids_pids=False,
     if not isinstance(uids, (np.ndarray, np.int32)):
       raise NotImplementedError(
           f'sid_pid from file mapping is only supported for np.ndarray and np.int32 for now. '
-          f'Found {type(uids)}.')
-    # spf2sp: contains 3/4-digit sid_pid mappings
+          f'Given uids of type: {type(uids)}.')
+    # spf2sp: contains sid_pid mappings
     spf2sp = experimental_dataset_spec._sid_pid_file2sid_pid
-    sids_pids = sids * dtype(10**2) + maximum(pids, dtype(0))
-    spf2sp__dense = _sparse_ids_mapping_to_dense_ids_mapping(spf2sp, -100, length=10000)
+    sids_pids = where(pids == noinfo_ids, sids, sids * dtype(10**2) + pids)
+    spf2sp__dense = _sparse_ids_mapping_to_dense_ids_mapping(spf2sp, -10**6, length=10000) # -10**6 random number
     sids_pids = spf2sp__dense[sids_pids]
-    assert not np.any(np.equal(sids_pids, -100)), (
-        'experimental_dataset_spec._sid_pid_file2sid_pid does not contain all pids in GT.')
-    pids = sids_pids % 100
-    pids = where(uids <= 99_999, invalid_ids, pids)
-
-  pids = where(pids == 0, invalid_ids, pids)
+    assert not np.any(np.equal(sids_pids, -10**6)), (
+        'experimental_dataset_spec._sid_pid_file2sid_pid does not contain all pids in GT files.')
+    pids = where(sids_pids >= 1_00, sids_pids % 100, noinfo_ids)
 
   if isinstance(uids, np.ndarray):
     sids = np.asarray(sids, dtype=np.int32)
@@ -195,13 +198,10 @@ def decode_uids(uids, *, return_sids_iids=False, return_sids_pids=False,
     returns += (sids_iids,)
 
   if return_sids_pids:
-    # TODO(panos): introduces 1_00 (zeros), is this preferred?
-    sids_pids = sids * dtype(10**2) + maximum(pids, dtype(0))
+    if 'sids_pids' not in locals(): # save some compute
+      sids_pids = where(pids == noinfo_ids, sids, sids * dtype(10**2) + pids)
     if isinstance(uids, np.ndarray):
       sids_pids = np.asarray(sids_pids, dtype=np.int32)
-    
-    sids_pids = where(divmod_(sids_pids, 100)[1] == 0, sids, sids_pids)
-
     returns += (sids_pids,)
 
   return returns
