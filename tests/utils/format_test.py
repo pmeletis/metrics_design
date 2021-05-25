df6d23db919bc88aedb02b8111ccfb52f0191960
pmeletis <p_c_meletis@yahoo.com>
2021-05-25 18:53:43 +0200

    Update decoding of labels with 2 experimental features: i) the range/values of sids, iids, sids_iids, pids, sids_pids can be now checked by providing the dataset_spec to the decode_uids function, ii) the part-level instance information layer can be automatically removed from the PASCAL dataset by providing the dataset_spec.

--- tests/utils/format_test.py
+++ tests/utils/format_test.py
@@ -2,16 +2,21 @@ import sys
 assert float(sys.version[:3]) >= 3.7, 'This test uses Python >= 3.7 functionality.'
 import functools
 import copy
+from typing import Dict
+from dataclasses import dataclass
+import glob
 
 import numpy as np
 import tensorflow as tf
 assert tf.version.VERSION[0] == '2', 'This test uses TF r2.x functionality.'
 import torch
 assert torch.version.__version__[0] == '1', 'This test uses PyTorch r1.x functionality.'
+from PIL import Image
 
 from panoptic_parts.utils.format import decode_uids, encode_ids
+from panoptic_parts.specs.dataset_spec import DatasetSpec
 
-# TOTAL_TESTS = 120 + 30 = 150 in this file.
+# TOTAL_TESTS = 128 + 30 = 158 in this file.
 
 def encode_ids_cases():
   examples = [[(1, 2, 3), (1_002_03,)],
@@ -88,15 +93,33 @@ def decode_uids_cases():
       functools.partial(torch.tensor, dtype=torch.int32)
   ]
 
-  class dataset_spec:
-    _sid_pid_file2sid_pid = {1_03: 1_04, 11_03: 11_05}
-  ersipTd = copy.copy(ersipT)
-  ersipTd['experimental_dataset_spec'] = dataset_spec
+  class DSpec:
+    # this is needed because deepcopy does not copy class-level attributes
+    def __init__(self):
+      self._sid_pid_file2sid_pid: Dict = {1_01: 1_02, 11_02: 11_01}
+      self.sid2scene_class = {k: str(k) if k != 0 else 'UNLABELED' for k in range(12)}
+      self.sid_pid2scene_class_part_class = {k: (v, 'UNLABELED') for k, v in self.sid2scene_class.items()}
+      self.sid_pid2scene_class_part_class.update({1_01: ('1', '1'), 1_02: ('1', '2'),
+                                                  11_01: ('11', '1'), 11_02: ('11', '2')})
+
+  dataset_spec1 = DSpec()
+  ersipTdc1 = copy.copy(ersipT)
+  ersipTdc1['experimental_dataset_spec'] = dataset_spec1
+  ersipTdc1['experimental_correct_range'] = True
+  dataset_spec2 = copy.deepcopy(dataset_spec1)
+  del dataset_spec2._sid_pid_file2sid_pid
+  ersipTdc2 = copy.copy(ersipT)
+  ersipTdc2['experimental_dataset_spec'] = dataset_spec2
+  ersipTdc2['experimental_correct_range'] = True
   more_examples = [
-      [[(1_002_03,), ersipT], (1, 2, 3, 1002, 103)],
-      [[(1_002_03,), ersipTd], (1, 2, 4, 1002, 104)],
-      [[(11_002_03,), ersipT], (11, 2, 3, 11002, 1103)],
-      [[(11_002_03,), ersipTd], (11, 2, 5, 11002, 1105)],
+      [[(1_002_01,), ersipT], (1, 2, 1, 1002, 1_01)],
+      [[(1_002_01,), ersipTdc1], (1, 2, 2, 1002, 1_02)],
+      [[(11_002_02,), ersipT], (11, 2, 2, 11002, 11_02)],
+      [[(11_002_02,), ersipTdc1], (11, 2, 1, 11002, 11_01)],
+      [[(12,), ersipTdc2], (0, -1, -1, 0, 0)],
+      [[(12_002_01,), ersipTdc2], (0, -1, -1, 0, 0)],
+      [[(12_002_04,), ersipTdc2], (0, -1, -1, 0, 0)],
+      [[(11_002_03,), ersipTdc2], (11, 2, -1, 11_002, 11)],
   ]
 
   # cases: [[[args, kwargs], outputs], ...]
@@ -143,6 +166,28 @@ def decode_uids_test(cases):
       raise ValueError(f'Case failed.\ncase:\ninputs: {case[0]}\nexpected results: {case[1]}\nreal results: {results}')
   print(f"decode_uids: {len(cases)} test cases completed successfully.")
 
+
+def read_and_decode_files():
+  cpp_paths = glob.glob('/home/panos/git/p.meletis/panoptic_parts_datasets/tests/tests_files/gtFinePanopticParts/*/*/*.tif')
+  cpp_spec = '/home/panos/git/github/pmeletis/metrics_design/panoptic_parts/panoptic_parts/specs/dataset_specs/cpp_datasetspec.yaml'
+  ppp_paths = glob.glob('/home/panos/git/p.meletis/panoptic_parts_datasets/tests/tests_files/pascal_panoptic_parts/labels_v2/*/*.tif')
+  ppp_spec = '/home/panos/git/github/pmeletis/metrics_design/panoptic_parts/panoptic_parts/specs/dataset_specs/ppp_datasetspec.yaml'
+
+  cpp_spec = DatasetSpec(cpp_spec)
+  ppp_spec = DatasetSpec(ppp_spec)
+
+  for fp in cpp_paths:
+    uids = np.asanyarray(Image.open(fp), dtype=np.int32)
+    decode_uids(uids, return_sids_iids=True, return_sids_pids=True,
+                experimental_dataset_spec=cpp_spec, experimental_correct_range=True)
+
+  for fp in ppp_paths:
+    uids = np.asanyarray(Image.open(fp), dtype=np.int32)
+    decode_uids(uids, return_sids_iids=True, return_sids_pids=True,
+                experimental_dataset_spec=ppp_spec, experimental_correct_range=True)
+
+
 if __name__ == "__main__":
   decode_uids_test(decode_uids_cases())
   encode_ids_test(encode_ids_cases())
+  read_and_decode_files()
